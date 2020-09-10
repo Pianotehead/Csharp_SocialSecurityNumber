b5a4518cc605ebeb0f9f433d1169a066cb44f779
Theodor Nordkvist <teddin2@outlook.com>
2020-09-10 21:03:12 +0200

    More code implemented in methods

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿// using Microsoft.Win32.SafeHandles;
-using System;
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace Csharp_SocialSecurityNumber
 {
@@ -7,10 +8,18 @@ namespace Csharp_SocialSecurityNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Usage from the terminal\n");
-            Console.WriteLine("1) Csharp_SocialSecurityNumber John Doe 890101-2010");
-            Console.WriteLine("   If no parameters are entered, the program will ask for them");
-            Console.WriteLine("   If too few parameters are entered, the user will have to type them all over again\n");
+            Console.WriteLine("Usage from the terminal. Input must be in one of the following formats:\n");
+            Console.WriteLine("1) Csharp_SocialSecurityNumber John Doe 890101-2010 or");
+            Console.WriteLine("2) Csharp_SocialSecurityNumber John Doe 19890101-2010\n");
+
+            Console.WriteLine("Note that you can enter an SSN for someone born 2000 or later, but if you enter");
+            Console.WriteLine("an SSN with a date of birth in the future, you'll be asked to correct your input\n");
+
+            Console.WriteLine("If no parameters are entered, the program will ask for them");
+            Console.WriteLine("If too few parameters are entered, you'll be asked to start all over again.\n");
+            Console.WriteLine("If the program is run from within Visual Studio or another code editor, with");
+            Console.WriteLine("no parameters, you will be asked to enter them manually.\n");
+
             string socialSecurityNumber = string.Empty;
             string firstName = string.Empty;
             string lastName = string.Empty;
@@ -26,148 +35,151 @@ namespace Csharp_SocialSecurityNumber
                 }
                 Console.Write("Please type your first name: ");
                 firstName = Console.ReadLine();
-                firstName = ValidateName(firstName);
                 Console.Write("Please type your last name: ");
                 lastName = Console.ReadLine();
-                lastName = ValidateName(lastName);
-                Console.Write("Please type your Social security number as YYMMDD-XXXX: ");
+                Console.Write("Please type your Social security number as [YY]YYMMDD-XXXX: ");
                 socialSecurityNumber = Console.ReadLine();
-                socialSecurityNumber = ValidateSSN(socialSecurityNumber);
             }
-            // The code works up to this line
-            else if (args.Length == 3)
+            else if (args.Length >= 3)
 
-            {   // Read the console input and validate it
-                firstName = ValidateName(args[0]);
-                lastName = ValidateName(args[1]);
-                socialSecurityNumber = ValidateSSN(args[2]);
+            {
+                firstName = args[0];
+                lastName = args[1];
+                socialSecurityNumber = args[2];
             }
-            string gender = "Female";
-            int age = 30;
+            firstName = RejectEmptyInput(firstName);
+            lastName = RejectEmptyInput(lastName);
+            socialSecurityNumber = RejectEmptyInput(socialSecurityNumber);
+            socialSecurityNumber = ValidateSSN(socialSecurityNumber);
 
-            int genderNumber = int.Parse(socialSecurityNumber.Substring(socialSecurityNumber.Length - 2, 1));
-            //Om substring(9, 1), inte bra, inga magiska nummer i koden!
-            if (genderNumber % 2 != 0)
-            {
-                gender = "Male";
-            }
-            int startIndex = 0;
-            if (socialSecurityNumber.Length == 11)
-            {
-                startIndex = 0;
-            }
-            else if (socialSecurityNumber.Length == 13)
-            {
-                startIndex = 2;
-            }
-            int bornYear = Convert.ToInt32(socialSecurityNumber.Substring(startIndex, 2)) + 1900;
-            int bornMonth = Convert.ToInt32(socialSecurityNumber.Substring(startIndex + 2, 2));
-            int bornDay = Convert.ToInt32(socialSecurityNumber.Substring(startIndex + 4, 2));
-            DateTime thisDay = DateTime.Today;
-
-            //Calculate age. No idea what to do with someone born 2000 or later
-            age = thisDay.Year - bornYear;
-            int livedMonths = thisDay.Month - bornMonth;
-            int livedDays = thisDay.Day - bornDay;
-            if (livedMonths < 0 || (livedMonths == 0 && livedDays < 0))
-            {
-                age--;
-            }
-            string generation = GetGeneration(bornYear);
+            string gender = GetGender(socialSecurityNumber);
+            int age = CalculateAge(socialSecurityNumber);
+            string generation = GetGeneration(age);
             const int LEFTJUSTIFY = -25;
             // OUTPUT
             Console.Clear();
             Console.WriteLine($"{"Name:",LEFTJUSTIFY} {firstName} {lastName}");
-            Console.WriteLine($"{"Social Security Number:", LEFTJUSTIFY} { socialSecurityNumber}");
+            Console.WriteLine($"{"Social Security Number:",LEFTJUSTIFY} { socialSecurityNumber}");
             Console.WriteLine($"{"Gender:",LEFTJUSTIFY} {gender}");
-            Console.WriteLine($"{"Age:",LEFTJUSTIFY} {age}");
+            if (age >= 0)
+            {
+                Console.WriteLine($"{"Age:",LEFTJUSTIFY} {age}");
+            }
             Console.WriteLine($"{"Generation:",LEFTJUSTIFY} {generation}");
             Console.SetCursorPosition(0, 20);
 
         }
 
-        public static string GetGeneration(int bornYear)
+        private static string GetGender(string socialSecurityNumber)
         {
-            string generation;
-            if (bornYear <= 1927 && bornYear >= 1901)
+            int genderNumber = int.Parse(socialSecurityNumber.Substring(socialSecurityNumber.Length - 2, 1));
+            if (genderNumber % 2 != 0)
             {
-                generation = "Greatest";
-            }
-            else if (bornYear <= 1945 && bornYear >= 1928)
-            {
-                generation = "Silent Generation";
-            }
-            else if (bornYear <= 1964 && bornYear >= 1946)
-            {
-                generation = "Baby Boomer";
-            }
-            else if (bornYear <= 1980 && bornYear >= 1965)
-            {
-                generation = "Generation X";
-            }
-            else if (bornYear <= 1996 && bornYear >= 1981)
-            {
-                generation = "Millennial";
+                return "Male";
             }
             else
             {
-                generation = "Zoomer";
+                return "Female";
             }
-
-            return generation;
         }
 
-        public static string ValidateName(string name)
+        public static string RejectEmptyInput(string str)
         {
-            while (String.IsNullOrWhiteSpace(name))
+            while(String.IsNullOrEmpty(str) || String.IsNullOrWhiteSpace(str))
             {
                 Console.Write("You entered nothing, or space(s). Please try again: ");
-                name = Console.ReadLine();
+                str = Console.ReadLine();
+            }
+            return str;
+        }
+
+        public static string GetGeneration(int age)
+        {
+            if (age >= 93)
+            {
+                return "Greatest";
+            }
+            else if (age <= 92 && age >= 75)
+            {
+                return "Silent Generation";
+            }
+            else if (age <= 74 && age >= 56)
+            {
+                return "Baby Boomer";
+            }
+            else if (age <= 55 && age >= 40)
+            {
+                return "Generation X";
+            }
+            else if (age <= 39 && age >= 24)
+            {
+                return "Millennial";
+            }
+            else if (age <= 23 && age >= 0)
+            {
+                return  "Zoomer";
+            }
+            else
+            {
+                return "This person has not yet been born";
             }
 
-            return name;
         }
 
         public static string ValidateSSN(string socialSecurityNumber)
         {
-            
-            bool validSSN = !String.IsNullOrEmpty(socialSecurityNumber);
-            bool rightSizeSSN = false;
-            bool isNumeric = false;
-            long temp;
-            string noHyphen;
 
-            if (validSSN)
+            string expr = @"^(19|20)?\d{2}(01|02|03|04|05|06|07|08|09|10|11|12)((0[1-9])|(1|2)[0-9]|(30|31))-\d{4}$";
+            Regex legalSSN = new Regex(expr);
+
+            bool wrongFormatSSN = !legalSSN.IsMatch(socialSecurityNumber);
+            while (wrongFormatSSN)
             {
-                rightSizeSSN = socialSecurityNumber.Length == 11 || socialSecurityNumber.Length == 13;
-                //Remove the hyphen(-) from SSN, and save as new string to check for mumericality
-                if (rightSizeSSN && socialSecurityNumber.Contains('-'))
-                {
-                    noHyphen = socialSecurityNumber.Remove(socialSecurityNumber.IndexOf('-'), 1);
-                    isNumeric = Int64.TryParse(noHyphen, out temp);
-                }
-                validSSN = rightSizeSSN && isNumeric;
-            }
-            while (!validSSN)
-            {
-                Console.WriteLine("Your input was not in the correct format, or empty");
-                Console.WriteLine("It has to be either as YYMMDD-XXXX or YYYYMMDD-XXXX");
+                Console.WriteLine("Your input was not in the correct format. It has to be entered as");
+                Console.WriteLine("either YYMMDD-XXXX or YYYYMMDD-XXXX");
                 Console.Write("Please try again: ");
                 socialSecurityNumber = Console.ReadLine();
-                validSSN = !String.IsNullOrEmpty(socialSecurityNumber);
-                if (validSSN)
-                {
-                    rightSizeSSN = socialSecurityNumber.Length == 11 || socialSecurityNumber.Length == 13;
-                    if (rightSizeSSN && socialSecurityNumber.Contains('-'))
-                    {
-                        noHyphen = socialSecurityNumber.Remove(socialSecurityNumber.IndexOf('-'), 1);
-                        isNumeric = Int64.TryParse(noHyphen, out temp);
-                    }
-                    validSSN = rightSizeSSN && isNumeric;
-                }
             }
 
             return socialSecurityNumber;
         }
+
+        public static int CalculateAge(string socialSecurityNumber)
+        {
+            
+            DateTime thisDay = DateTime.Today;
+            bool shortSSN = socialSecurityNumber.Length == 11;
+            bool longSSN = socialSecurityNumber.Length == 13;
+            DateTime birthDate = new DateTime();
+            int age = 0;
+            if (longSSN)
+            {
+                birthDate = DateTime.ParseExact(socialSecurityNumber.Substring(0, 8), "yyyyMMdd", CultureInfo.CurrentCulture);
+                age = thisDay.Year - birthDate.Year;
+            }
+            else if (shortSSN)
+            {
+                birthDate = DateTime.ParseExact(socialSecurityNumber.Substring(0, 6), "yyMMdd", CultureInfo.CurrentCulture);
+                // With short SSN, I want to assume a person born in the 20th century
+                if (birthDate.Year < 2099 && birthDate.Year > DateTime.Now.Year)
+                {
+                    age = thisDay.Year - birthDate.Year + 100;
+                }
+                else
+                {
+                    age = thisDay.Year - birthDate.Year;
+                }
+                
+            }
+            int livedMonths = thisDay.Month - birthDate.Month;
+            int livedDays = thisDay.Day - birthDate.Day;
+            if (livedMonths < 0 || (livedMonths == 0 && livedDays < 0))
+            {
+                age--;
+            }
+            return age;
+        }
+
     }
 }
+
--- README.md
+++ README.md
@@ -1,9 +1,34 @@
-# Social Security Number
+## Social Security Number
 
-Console app that lets a user enter a social security number, 
+Console app that lets a user enter a social security number (SSN), 
 upon which the gender and age of the person is reported.
 
-"" Getting started
+# Getting started
 
 1. Clone the repository.
 2. Run solution.
+
+# Running the program
+
+If the app is run from the terminal,
+input must be in one of the following formats:
+
+1) Csharp_SocialSecurityNumber John Doe 890101-2010 or
+2) Csharp_SocialSecurityNumber John Doe 19890101-2010
+
+If you want to find out the age of someone born 2000 or later,
+you must enter an SSN in the long format (13 characters).
+
+If you enter an SSN in shorter format, 11 characters,
+the program will assume that person is born in the 20th century
+
+If you enter an SSN with a date (YYYYMMDD) in the future,
+the program will run, but age will not be published,
+and under the heading Generation, the program will show
+as output, that the person has not yet been born.
+
+If no parameters are entered, the program will ask for them
+If too few parameters are entered, you'll be asked to start all over again.
+
+If the program is run from within Visual Studio or another code editor, with
+no parameters, you will be asked to enter them manually.
